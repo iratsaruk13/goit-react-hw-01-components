d20ede6e1f8cda0235627a9efebb90bdd017599a
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-04-30 18:02:51 +0300

    1 task with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import  {Profile}  from "./components/Profile/Profile";
-import userdata from './components/Profile/user.json'
+import { Profile } from "./components/Profile/Profile";
+import userdata from "./components/Profile/user.json";
 
 function App() {
-    const {username, avatar, tag, location, stats} = userdata
-    return (
+  const { username, avatar, tag, location, stats } = userdata;
+  return (
     <>
-    <Profile
-    avatar={avatar}
-    username={username}
-    tag={tag}
-    location={location}
-    stats={stats}
-    />
+      <Profile
+        avatar={avatar}
+        username={username}
+        tag={tag}
+        location={location}
+        stats={stats}
+      />
     </>
-    )
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './components/Profile/user.json'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./components/Profile/user.json";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(<App />);
-
