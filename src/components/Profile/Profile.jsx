ac3e8cba6724765af862fd4e5ee4ecdc76cb382e
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-04-30 17:50:54 +0300

    add 1 task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import {Container, UserProfile, Avatar, UserName, UserTag, Location, StatsList, StatsItem, StatsLabel, StatsQuantity} from './Profile.styled';
+export const Profile = ({username, avatar, tag, location, stats}) => {
+    return (
+<Container key={username}>
+  <UserProfile>
+    <Avatar
+      src={avatar}
+      alt={username}
+    />
+    <UserName>{username}</UserName>
+    <UserTag>@{tag}</UserTag>
+    <Location>{location}</Location>
+  
+
+  <StatsList>
+    <StatsItem>
+      <StatsLabel>Followers</StatsLabel>
+      <StatsQuantity>{stats.followers}</StatsQuantity>
+    </StatsItem>
+    <StatsItem>
+      <StatsLabel>Views</StatsLabel>
+      <StatsQuantity>{stats.views}</StatsQuantity>
+    </StatsItem>
+    <StatsItem>
+      <StatsLabel>Likes</StatsLabel>
+      <StatsQuantity>{stats.likes}</StatsQuantity>
+    </StatsItem>
+  </StatsList>
+  </UserProfile>
+</Container>
+    )
+}
+
+Profile.prototype = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number.isRequired)
+}
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+margin-left: auto;
+margin-right: auto;
+background-color: honeydew;
+`
+
+const UserProfile = styled.div`
+text-align: center;
+background-color: #fff;
+max-width: 500px;
+margin: 0px auto;
+border-radius: 16px;
+transition: box-shadow 250ms cubic-bezier(.42, 0, .58, 1);
+  :hover {
+    box-shadow: 0 2px 5px rgba(0,0,0,0.2), 0 4px 6px rgba(0,0,0,0.2);;
+  }
+`
+
+const Avatar = styled.img`
+max-width: 150px;
+border-radius: 50%;
+background-color: honeydew;
+`
+const UserName = styled.p`
+font-size: 28px;
+font-weight: 700;
+`
+
+const UserTag = styled.p`
+font-size: 18px;
+font-weight: 500;
+color: rgb(78, 77, 77);
+`
+const Location = styled.p`
+font-size: 20px;
+font-weight: 400;
+color: rgb(78, 77, 77);
+`
+const StatsList = styled.ul`
+list-style: none;
+display: flex;
+justify-content: space-around;
+gap: 10px;
+  }`
+
+const StatsItem = styled.li`
+display: flex;
+flex-direction: column;
+flex-basis: calc(100% / 3);
+background-color: honeydew;
+border-radius: 16px;
+transition: box-shadow 250ms cubic-bezier(.42, 0, .58, 1);
+  :hover {
+    box-shadow: 0 2px 5px rgba(0,0,0,0.2), 0 4px 6px rgba(0,0,0,0.2);
+`
+
+const StatsLabel = styled.span`
+font-size: 20px;
+font-weight: 500;
+color: rgb(78, 77, 77);
+`
+const StatsQuantity = styled.span`
+font-size: 32px;
+font-weight: 700;
+color: rgb(78, 77, 77);
+`
+
+
+export {Container, UserProfile, Avatar, UserName, UserTag, Location, StatsList, StatsItem, StatsLabel, StatsQuantity}
